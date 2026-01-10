668ce67e7f1dfdb6bd614573744c6fb330978fd4
Aleksandr Turchenko <alexandrt100@gmail.com>
2026-01-10 22:22:08 +0100

    create OrderRepository abstract interfase

--- 05/src/1/order-service.ts
+++ 05/src/1/order-service.ts
@@ -3,14 +3,26 @@ type Order = {
   items: string[];
 };
 
+interface OrderRepository {
+  save(order: Order): Promise<void>;
+}
+
+class OrderApiRepository implements OrderRepository {
+  async save(order: Order) {
+    await fetch("https://api.business.com/order", {
+      method: "POST",
+      body: JSON.stringify(order),
+    });
+  }
+}
+
 class OrderService {
+  constructor(private readonly repository: OrderRepository) {}
+
   async create(orderData: Order) {
     if (orderData.items.length === 0) throw new Error("Carrito vacío");
 
-    await fetch("https://api.business.com/order", {
-      method: "POST",
-      body: JSON.stringify(orderData),
-    });
+    await this.repository.save(orderData);
 
     await fetch("https://hooks.slack.com/services/...", {
       method: "POST",
